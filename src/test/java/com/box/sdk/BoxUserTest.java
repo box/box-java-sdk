543ba8760a1f216b7b7deabfbd770ca9e6610d92
Greg Curtis <greg.r.curtis@gmail.com>
2014-08-28 13:23:53 -0700

    Add BoxUser class
    Includes:
    
    * Getting the current user
    * Getting a user's info

---
+++ src/test/java/com/box/sdk/BoxUserTest.java
@@ -0,0 +1,20 @@
+package com.box.sdk;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class BoxUserTest {
+    @Test
+    @Category(IntegrationTest.class)
+    public void getCurrentUserInfoIsCorrect() throws InterruptedException {
+        final String expectedName = "Java SDK";
+        BoxAPIConnection api = new BoxAPIConnection(TestConfig.getAuthToken());
+        BoxUser user = BoxUser.getCurrentUser(api);
+        BoxUser.Info info = user.getInfo();
+
+        assertThat(info.getName(), equalTo(expectedName));
+    }
+}
