7b8101264a74159f6b87be3883dcc19738160bd5
Greg Curtis <greg.r.curtis@gmail.com>
2014-09-18 00:48:27 -0700

    Add documentation to BoxAPIRequest

--- src/main/java/com/box/sdk/BoxAPIRequest.java
+++ src/main/java/com/box/sdk/BoxAPIRequest.java
@@ -14,6 +14,23 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Used to make HTTP requests to the Box API.
+ *
+ * <p>All communication with the REST API is done through this class or one of its subclasses. This class wraps {@link
+ * HttpURLConnection} in order to provide a simpler interface that can automatically handle various conditions specific
+ * to Box's API. Requests will be authenticated using a {@link BoxAPIConnection} (if one is provided), so it isn't
+ * necessary to add authorization headers. Requests can also be sent more than once, unlike with HttpURLConnection. If
+ * an error occurs while sending a request, it will be automatically retried (with a back off delay) up to the maximum
+ * number of times set in the BoxAPIConnection.</p>
+ *
+ * <p>Specifying a body for a BoxAPIRequest is done differently than it is with HttpURLConnection. Instead of writing to
+ * an OutputStream, the request is provided an {@link InputStream} which will be read when the {@link #send} method is
+ * called. This makes it easy to retry requests since the stream can automatically reset and reread with each attempt.
+ * If the stream cannot be reset, then a new stream will need to be provided before each call to send. There is also a
+ * convenience method for specifying the body as a String, which simply wraps the String with an InputStream.</p>
+ *
+ */
 public class BoxAPIRequest {
     private static final Logger LOGGER = Logger.getLogger(BoxAPIRequest.class.getName());
 
@@ -28,10 +45,21 @@ public class BoxAPIRequest {
     private long bodyLength;
     private Map<String, List<String>> requestProperties;
 
+    /**
+     * Constructs an unauthenticated BoxAPIRequest.
+     * @param  url    the URL of the request.
+     * @param  method the HTTP method of the request.
+     */
     public BoxAPIRequest(URL url, String method) {
         this(null, url, method);
     }
 
+    /**
+     * Constructs an authenticated BoxAPIRequest using a provided BoxAPIConnection.
+     * @param  api    an API connection for authenticating the request.
+     * @param  url    the URL of the request.
+     * @param  method the HTTP method of the request.
+     */
     public BoxAPIRequest(BoxAPIConnection api, URL url, String method) {
         this.api = api;
         this.url = url;
@@ -43,27 +71,64 @@ public class BoxAPIRequest {
         this.addHeader("Accept-Charset", "utf-8");
     }
 
+    /**
+     * Adds an HTTP header to this request.
+     * @param key   the header key.
+     * @param value the header value.
+     */
     public void addHeader(String key, String value) {
         this.headers.add(new RequestHeader(key, value));
     }
 
+    /**
+     * Sets a timeout for this request in milliseconds.
+     * @param timeout the timeout in milliseconds.
+     */
     public void setTimeout(int timeout) {
         this.timeout = timeout;
     }
 
-    public void setContentLength(long length) {
-        this.bodyLength = length;
-    }
-
+    /**
+     * Sets the request body to the contents of an InputStream.
+     *
+     * <p>The stream must support the {@link InputStream#reset} method if auto-retry is used or if the request needs to
+     * be resent. Otherwise, the body must be manually set before each call to {@link #send}.
+     *
+     * @param stream an InputStream containing the contents of the body.
+     */
     public void setBody(InputStream stream) {
         this.body = stream;
     }
 
+    /**
+     * Sets the request body to the contents of a String.
+     *
+     * <p>If the contents of the body are large, then it may be more efficient to use an {@link InputStream} instead of
+     * a String. Using a String requires that the entire body be in memory before sending the request.</p>
+     *
+     * @param body a String containing the contents of the body.
+     */
     public void setBody(String body) {
         this.bodyLength = body.length();
         this.body = new ByteArrayInputStream(body.getBytes(StandardCharsets.UTF_8));
     }
 
+    /**
+     * Sends this request and returns a BoxAPIResponse containing the server's response.
+     *
+     * <p>The type of the returned BoxAPIResponse will be based on the content type returned by the server, allowing it
+     * to be cast to a more specific type. For example, if it's known that the API call will return a JSON response,
+     * then it can be cast to a {@link BoxJSONResponse} like so:</p>
+     *
+     * <pre>BoxJSONResponse response = (BoxJSONResponse) request.send();</pre>
+     *
+     * <p>If the server returns an error code or if a network error occurs, then the request will be automatically
+     * retried. If the maximum number of retries is reached and an error still occurs, then a {@link BoxAPIException}
+     * will be thrown.</p>
+     *
+     * @throws BoxAPIException if the server returns an error code or if a network error occurs.
+     * @return a {@link BoxAPIResponse} containing the server's response.
+     */
     public BoxAPIResponse send() {
         if (this.api == null) {
             this.backoffCounter.reset(BoxAPIConnection.DEFAULT_MAX_ATTEMPTS);
@@ -97,6 +162,10 @@ public class BoxAPIRequest {
         throw new RuntimeException();
     }
 
+    /**
+     * Returns a String containing the URL, HTTP method, headers and body of this request.
+     * @return a String containing information about this request.
+     */
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
@@ -143,10 +212,27 @@ public class BoxAPIRequest {
         this.backoffCounter = counter;
     }
 
+    /**
+     * Returns a String representation of this request's body used in {@link #toString}. This method returns
+     * null by default.
+     *
+     * <p>A subclass may want override this method if the body can be converted to a String for logging or debugging
+     * purposes.</p>
+     *
+     * @return a String representation of this request's body.
+     */
     protected String bodyToString() {
         return null;
     }
 
+    /**
+     * Writes the body of this request to an HttpURLConnection.
+     *
+     * <p>Subclasses overriding this method must remember to close the connection's OutputStream after writing.</p>
+     *
+     * @param connection the connection to which the body should be written.
+     * @throws BoxAPIException if an error occurs while writing to the connection.
+     */
     protected void writeBody(HttpURLConnection connection) {
         if (this.body == null) {
             return;
@@ -166,6 +252,14 @@ public class BoxAPIRequest {
         }
     }
 
+    /**
+     * Resets the InputStream containing this request's body.
+     *
+     * <p>This method will be called before each attempt to resend the request, giving subclasses an opportunity to
+     * reset any streams that need to be read when sending the body.</p>
+     *
+     * @throws IOException if the stream cannot be reset.
+     */
     protected void resetBody() throws IOException {
         if (this.body != null) {
             this.body.reset();
