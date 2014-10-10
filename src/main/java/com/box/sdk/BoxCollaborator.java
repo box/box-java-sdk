a168b38dfcc434cebf6f9ffad355964080f67e5e
Greg Curtis <greg.r.curtis@gmail.com>
2014-10-10 16:05:40 -0700

    Add javadocs for BoxCollaborator

--- src/main/java/com/box/sdk/BoxCollaborator.java
+++ src/main/java/com/box/sdk/BoxCollaborator.java
@@ -6,41 +6,82 @@ import java.util.Date;
 import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
 
+/**
+ * The abstract base class for types that can be added to collaborations.
+ */
 public abstract class BoxCollaborator extends BoxResource {
+
+    /**
+     * Constructs a BoxCollaborator for a collaborator with a given ID.
+     * @param  api the API connection to be used by the collaborator.
+     * @param  id  the ID of the collaborator.
+     */
     public BoxCollaborator(BoxAPIConnection api, String id) {
         super(api, id);
     }
 
+    /**
+     * Contains additional information about a BoxCollaborator.
+     *
+     * @param <T> the type of the collaborator associated with this info.
+     */
     public abstract class Info<T extends BoxCollaborator> extends BoxResource.Info<T> {
         private String name;
         private Date createdAt;
         private Date modifiedAt;
 
+        /**
+         * Constructs an empty Info object.
+         */
         public Info() {
             super();
         }
 
+        /**
+         * Constructs an Info object by parsing information from a JSON string.
+         * @param  json the JSON string to parse.
+         */
         public Info(String json) {
             super(json);
         }
 
+        /**
+         * Constructs an Info object using an already parsed JSON object.
+         * @param  jsonObject the parsed JSON object.
+         */
         protected Info(JsonObject jsonObject) {
             super(jsonObject);
         }
 
-        public String name() {
+        /**
+         * Gets the name of the collaborator.
+         * @return the name of the collaborator.
+         */
+        public String getName() {
             return this.name;
         }
 
+        /**
+         * Sets the name of the collaborator.
+         * @param name the new name of the collaborator.
+         */
         public void setName(String name) {
             this.name = name;
             this.addPendingChange("name", name);
         }
 
+        /**
+         * Gets the date that the collaborator was created.
+         * @return the date that the collaborator was created.
+         */
         public Date getCreatedAt() {
             return this.createdAt;
         }
 
+        /**
+         * Gets the date that the collaborator was modified.
+         * @return the date that the collaborator was modified.
+         */
         public Date getModifiedAt() {
             return this.modifiedAt;
         }
