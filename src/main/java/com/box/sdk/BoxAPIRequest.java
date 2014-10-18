ec784a9671ca41b4f702d238ffb0871fa04f3a96
Greg Curtis <greg.r.curtis@gmail.com>
2014-10-17 18:35:57 -0700

    Add custom user agent to all HTTP requests
    All requests will include a custom user agent header indicating the SDK
    name and version. For example:
    
            User-Agent: Box Java SDK v0.3
    
    Closes #34.

--- src/main/java/com/box/sdk/BoxAPIRequest.java
+++ src/main/java/com/box/sdk/BoxAPIRequest.java
@@ -304,6 +304,8 @@ public class BoxAPIRequest {
 
     private BoxAPIResponse trySend(ProgressListener listener) {
         HttpURLConnection connection = this.createConnection();
+        connection.setRequestProperty("User-Agent", "Box Java SDK v0.3");
+
         if (this.bodyLength > 0) {
             connection.setFixedLengthStreamingMode(this.bodyLength);
             connection.setDoOutput(true);
