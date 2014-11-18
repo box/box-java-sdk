eb92ec8a242b845a88ef1b97aa86afa197b0594d
Greg Curtis <greg.r.curtis@gmail.com>
2014-11-18 15:19:05 -0800

    Add javadocs to BoxUploadEmail

--- src/main/java/com/box/sdk/BoxUploadEmail.java
+++ src/main/java/com/box/sdk/BoxUploadEmail.java
@@ -3,12 +3,22 @@ package com.box.sdk;
 import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
 
+/**
+ * Represents an email address that can be used to upload files to a folder on Box.
+ */
 public class BoxUploadEmail extends BoxJSONObject {
     private Access access;
     private String email;
 
+    /**
+     * Constructs a BoxUploadEmail with default settings.
+     */
     public BoxUploadEmail() { }
 
+    /**
+     * Constructs a BoxUploadEmail from a JSON string.
+     * @param  json the JSON encoded upload email.
+     */
     public BoxUploadEmail(String json) {
         super(json);
     }
@@ -17,15 +27,27 @@ public class BoxUploadEmail extends BoxJSONObject {
         super(jsonObject);
     }
 
+    /**
+     * Gets the access level of this upload email.
+     * @return the access level of this upload email.
+     */
     public Access getAccess() {
         return this.access;
     }
 
+    /**
+     * Sets the access level of this upload email.
+     * @param access the new access level of this upload email.
+     */
     public void setAccess(Access access) {
         this.access = access;
         this.addPendingChange("access", access.toJSONValue());
     }
 
+    /**
+     * Gets the email address of this upload email.
+     * @return the email address of this upload email.
+     */
     public String getEmail() {
         return this.email;
     }
@@ -44,8 +66,18 @@ public class BoxUploadEmail extends BoxJSONObject {
         }
     }
 
+    /**
+     * Enumerates the possible access levels that can be set on an upload email.
+     */
     public enum Access {
+        /**
+         * Anyone can send an upload to this email address.
+         */
         OPEN("open"),
+
+        /**
+         * Only collaborators can send an upload to this email address.
+         */
         COLLABORATORS("collaborators");
 
         private final String jsonValue;
@@ -54,11 +86,11 @@ public class BoxUploadEmail extends BoxJSONObject {
             this.jsonValue = jsonValue;
         }
 
-        public static Access fromJSONValue(String jsonValue) {
+        static Access fromJSONValue(String jsonValue) {
             return Access.valueOf(jsonValue.toUpperCase());
         }
 
-        public String toJSONValue() {
+        String toJSONValue() {
             return this.jsonValue;
         }
     }
