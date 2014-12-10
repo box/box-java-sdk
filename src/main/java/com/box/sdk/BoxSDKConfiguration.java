c7d21ff88630dd5fc037bc53d40baf18278147d6
Sarat Tallamraju <sarat@box.com>
2014-12-10 12:57:42 -0800

    Added a static BoxSDKConfiguration class to be able to configure User-Agent
    In the future, the BoxSDKConfiguration can be used to enable custom user configurations.

---
+++ src/main/java/com/box/sdk/BoxSDKConfiguration.java
@@ -0,0 +1,29 @@
+package com.box.sdk;
+
+/**
+ * Contains all the default configurations for the Box SDK that can be modified by the user.
+ */
+public class BoxSDKConfiguration {
+
+    // Defaults here
+    private static final String DEFAULT_USER_AGENT = "Box Java SDK v0.4";
+
+    // User settings.
+    private static String userAgent = DEFAULT_USER_AGENT;
+
+    /**
+     * Returns the user agent header. 
+     * @return the user agent header.
+     */
+    public static String getUserAgent() {
+        return userAgent;
+    }
+    
+    /**
+     * Overrides the default user agent with the specified {@code userAgent}.
+     * @param userAgent
+     */
+    public static void setUserAgent(String userAgent) {
+        BoxSDKConfiguration.userAgent = userAgent;
+    }
+}
