5eb0e75b5b107d91254f12ff0013139e816fd061
Sarat Tallamraju <sarat@box.com>
2014-12-10 13:09:19 -0800

    Removed trailing spaces

--- src/main/java/com/box/sdk/BoxSDKConfiguration.java
+++ src/main/java/com/box/sdk/BoxSDKConfiguration.java
@@ -5,20 +5,20 @@ package com.box.sdk;
  */
 public class BoxSDKConfiguration {
 
-    // Defaults here
+    // Defaults here.
     private static final String DEFAULT_USER_AGENT = "Box Java SDK v0.4";
 
     // User settings.
     private static String userAgent = DEFAULT_USER_AGENT;
 
     /**
-     * Returns the user agent header. 
+     * Returns the user agent header.
      * @return the user agent header.
      */
     public static String getUserAgent() {
         return userAgent;
     }
-    
+
     /**
      * Overrides the default user agent with the specified {@code userAgent}.
      * @param userAgent the user agent to send with API requests.
