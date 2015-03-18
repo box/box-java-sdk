f511501dc74e60452fa26acdec8c3390f8e6bf48
Greg Curtis <greg.r.curtis@gmail.com>
2015-03-17 20:59:14 -0700

    Add EventLog for retrieving a static log of events
    The EventLog class provides methods for retrieving static logs of
    events. Unlike the EventStream, an EventLog isn't updated in real-time.
    For now it only provides a way of retrieving enterprise events, since
    those aren't available in real-time via EventStream.
    
    It's also worth noting that there's a bug in the Box events endpoint
    where some enterprise event sources don't map correctly to a
    BoxResource. In the cases where this happens, the `getSourceInfo()` will
    return null, and a new a `getSourceJSON()` method can be used to access
    the event source JSON directly.
    
    Closes #81.

---
+++ src/test/java/com/box/sdk/EventLogTest.java
@@ -0,0 +1,26 @@
+package com.box.sdk;
+
+import java.util.Date;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class EventLogTest {
+    @Test
+    @Category(IntegrationTest.class)
+    public void getEnterpriseEventsReturnsAtLeastOneEvent() {
+        BoxAPIConnection api = new BoxAPIConnection(TestConfig.getAccessToken());
+        Date after = new Date(0L);
+        Date before = new Date(System.currentTimeMillis());
+        EventLog events = EventLog.getEnterpriseEvents(api, after, before);
+
+        assertThat(events.getSize(), is(not(0)));
+        assertThat(events.getStartDate(), is(equalTo(after)));
+        assertThat(events.getEndDate(), is(equalTo(before)));
+    }
+}
