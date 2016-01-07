d72667b636146b99f2ca79c2b3f4cf85c9d845c1
Greg Curtis <gcurtis@box.com>
2016-01-07 13:34:06 -0800

    Bump version for v2.0.0 release

--- src/main/java/com/box/sdk/BoxAPIConnection.java
+++ src/main/java/com/box/sdk/BoxAPIConnection.java
@@ -83,7 +83,7 @@ public class BoxAPIConnection {
         this.autoRefresh = true;
         this.maxRequestAttempts = DEFAULT_MAX_ATTEMPTS;
         this.refreshLock = new ReentrantReadWriteLock();
-        this.userAgent = "Box Java SDK v1.2.0-SNAPSHOT";
+        this.userAgent = "Box Java SDK v2.0.0";
         this.listeners = new ArrayList<BoxAPIConnectionListener>();
     }
 
