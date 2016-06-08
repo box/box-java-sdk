e08bd3a560380fb5e3b9eb16019c3bebeaf9233a
Tarrence van As <tarrence@box.com>
2016-06-08 15:21:03 -0700

    Format date as string

--- src/main/java/com/box/sdk/BoxTask.java
+++ src/main/java/com/box/sdk/BoxTask.java
@@ -185,7 +185,7 @@ public class BoxTask extends BoxResource {
          */
         public void setDueAt(Date dueAt) {
             this.dueAt = dueAt;
-            this.addPendingChange("due_at", dueAt);
+            this.addPendingChange("due_at", dueAt.toString());
         }
 
         /**
