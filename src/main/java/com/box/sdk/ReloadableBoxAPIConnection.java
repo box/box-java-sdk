d44ae1f72797c4f26b6f08e18fe1d804d5488ed6
Tabs <ryan.cooke@gmail.com>
2016-12-23 13:25:52 -0500

    user event log do not take a types parameter

--- src/main/java/com/box/sdk/ReloadableBoxAPIConnection.java
+++ src/main/java/com/box/sdk/ReloadableBoxAPIConnection.java
@@ -1,19 +1,9 @@
 package com.box.sdk;
 
-import com.eclipsesource.json.JsonObject;
-
-import java.net.MalformedURLException;
-import java.net.Proxy;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-
 /**
  * Represents an authenticated connection to the Box API.
  *
- * <p>This class handles storing authentication information, automatic token refresh, and rate-limiting. It can also be
+ * <p>This class handles storing authentication information, token refresh, and rate-limiting. It can also be
  * used to configure the Box API endpoint URL in order to hit a different version of the API. Multiple instances of
  * BoxAPIConnection may be created to support multi-user login.</p>
  */
@@ -30,13 +20,10 @@ public class ReloadableBoxAPIConnection extends BoxAPIConnection {
     }
 
     /**
-     * Gets an access token that can be used to authenticate an API request. This method will automatically refresh the
-     * access token if it has expired since the last call to <code>getAccessToken()</code>.
-     * @return a valid access token that can be used to authenticate an API request.
+     * Gets an access token that can be used to authenticate an API request.
+     * @return an access token that can be used to authenticate an API request.
      */
     public String getAccessToken() { return this.accessToken; }
 
-    public boolean needsRefresh() {
-        return false;
-    }
+    public boolean needsRefresh() { return false; }
 }
