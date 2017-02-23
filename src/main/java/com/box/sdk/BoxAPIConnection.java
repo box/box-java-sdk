3493c8c030f1a11844e503d109f9187ceb25fadb
David Maynard <dmaynard@box.com>
2017-02-23 15:36:54 -0800

    Updated version number to 2.4.0 since we have a new createMetadata endpoint

--- src/main/java/com/box/sdk/BoxAPIConnection.java
+++ src/main/java/com/box/sdk/BoxAPIConnection.java
@@ -83,7 +83,7 @@ public class BoxAPIConnection {
         this.autoRefresh = true;
         this.maxRequestAttempts = DEFAULT_MAX_ATTEMPTS;
         this.refreshLock = new ReentrantReadWriteLock();
-        this.userAgent = "Box Java SDK v2.3.1";
+        this.userAgent = "Box Java SDK v2.4.0";
         this.listeners = new ArrayList<BoxAPIConnectionListener>();
     }
 
