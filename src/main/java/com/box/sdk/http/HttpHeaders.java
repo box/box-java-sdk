ed0ecab538268d55de308ff674fbeaaa7ae4d6f2
Karthik Shanmugasundaram <kshanmugasundaram@box.com>
2017-02-27 12:01:45 -0800

    If-Match and If-None-Match headers implementation

--- src/main/java/com/box/sdk/http/HttpHeaders.java
+++ src/main/java/com/box/sdk/http/HttpHeaders.java
@@ -9,6 +9,8 @@ public final class HttpHeaders {
     public static final String CONTENT_RANGE = "Content-Range";
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String DIGEST = "Digest";
+    public static final String IF_MATCH = "If-Match";
+    public static final String IF_NONE_MATCH = "If-None-Match";
     public static final String X_BOX_PART_ID = "X-Box-Part-Id";
 
     //Prevents instantiation
