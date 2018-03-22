3dd440c2455c0ef9fa3126839a5515262f22bc04
Matt Willer <mwiller@box.com>
2018-03-22 00:28:01 -0700

    Remove stray line in test

--- src/main/java/com/box/sdk/BoxFile.java
+++ src/main/java/com/box/sdk/BoxFile.java
@@ -960,8 +960,6 @@ public class BoxFile extends BoxItem {
         JsonValue lockValue = responseJSON.get("lock");
         JsonObject lockJSON = JsonObject.readFrom(lockValue.toString());
 
-        BoxTermsOfService.TermsOfServiceType.EXTERNAL
-
         return new BoxLock(lockJSON, this.getAPI());
     }
 
