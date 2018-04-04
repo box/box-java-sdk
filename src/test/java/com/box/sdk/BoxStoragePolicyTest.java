6529e268692b41030ccfe52ab2672e9aac0661ac
Cary Cheng <ccheng@box.com>
2018-04-04 13:44:06 -0700

    storage policy test

--- src/test/java/com/box/sdk/BoxStoragePolicyTest.java
+++ src/test/java/com/box/sdk/BoxStoragePolicyTest.java
@@ -1,28 +1,56 @@
 package com.box.sdk;
 
+import java.net.URL;
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-import com.eclipsesource.json.ParseException;
-import com.github.tomakehurst.wiremock.common.Json;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-import com.eclipsesource.json.JsonObject;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.delete;
+import static com.github.tomakehurst.wiremock.client.WireMock.deleteRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 
-import javax.swing.*;
-import java.util.Iterator;
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 /**
  * {@link BoxStoragePolicy} related tests.
  */
 public class BoxStoragePolicyTest {
 
+	/**
+	 * Wiremock
+	 */
+	@Rule
+	public WireMockRule wireMockRule = new WireMockRule(53620);
+
 	@Test
 	@Category(UnitTest.class)
 	public void testGetInfoParseAllFieldsCorrectly() throws ParseException {
+		BoxAPIConnection api = new BoxAPIConnection("");
+  		api.setBaseURL("http://localhost:53620/");
+
 		final String storagePolicyID = "1234";
 		final String storagePolicyName = "AWS Frankfurt / AWS Dublin with in region Uploads/Downloads/Previews";
 
@@ -32,8 +60,10 @@ public class BoxStoragePolicyTest {
 						+ "    \"name\": \"AWS Frankfurt / AWS Dublin with in region Uploads/Downloads/Previews\"\n"
 		                + "}");
 
-		BoxAPIConnection api = new BoxAPIConnection("");
-		api.setRequestInterceptor(JSONRequestInterceptor.respondWith(fakeJSONResponse));
+		stubFor(get(urlEqualTo("/storage_policies"))
+	  		.willReturn(aResponse()
+				.withHeader("Content-Type", "application/json")
+				.withBody(fakeJSONResponse.toString())));
 
 		BoxStoragePolicy storagePolicy = new BoxStoragePolicy(api, storagePolicyID);
 		BoxStoragePolicy.Info storagePolicyInfo = storagePolicy.getInfo();
@@ -44,6 +74,9 @@ public class BoxStoragePolicyTest {
 	@Test
 	@Category(UnitTest.class)
 	public void testGetStoragePoliciesParseAllFieldsCorrectly() throws ParseException {
+		BoxAPIConnection api = new BoxAPIConnection("");
+  		api.setBaseURL("http://localhost:53620/");
+
 		final String storagePolicyType = "storage_policy";
 		final String firstStoragePolicyID = "1234";
 		final String firstStoragePolicyName = "AWS Montreal / AWS Dublin";
@@ -51,7 +84,7 @@ public class BoxStoragePolicyTest {
 		final String secondStoragePolicyName = "AWS Frankfurt / AWS Dublin with in region Uploads/Downloads/Previews";
 
 		final JsonObject fakeJSONResponse = JsonObject.readFrom("{\n"
-          + "    \"limit\": 1000,\n"
+          + "    \"limit\": 100,\n"
 		  + "    \"next_marker\": null,\n"
           + "    \"entries\": [\n"
           + "        {\n"
@@ -67,8 +100,10 @@ public class BoxStoragePolicyTest {
           + "    ]\n"
           + "}");
 
-		BoxAPIConnection api = new BoxAPIConnection("");
-		api.setRequestInterceptor(JSONRequestInterceptor.respondWith(fakeJSONResponse));
+		stubFor(get(urlEqualTo("/storage_policies?limit=100"))
+	  		.willReturn(aResponse()
+				.withHeader("Content-Type", "application/json")
+				.withBody(fakeJSONResponse.toString())));
 
 		Iterator<BoxStoragePolicy.Info> storagePolcies = BoxStoragePolicy.getAll(api).iterator();
 		BoxStoragePolicy.Info firstStoragePolicyInfo = storagePolcies.next();
