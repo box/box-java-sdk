bd01b60aceb4ff59b6375bde11751e2dcc41772e
Cary Cheng <ccheng@box.com>
2018-04-05 13:14:46 -0700

    first implementation of exceptions

--- src/main/java/com/box/sdk/BoxAPIResponseException.java
+++ src/main/java/com/box/sdk/BoxAPIResponseException.java
@@ -3,7 +3,49 @@ package com.box.sdk;
 /**
  * Thrown to indicate than an error occured while returning with a response from the Box API.
  */
-public class BoxAPIResponseException {
+public class BoxAPIResponseException extends RuntimeException{
 
+	private final String message;
+	private final String response;
+	private BoxAPIResponse responseObj;
+
+	/**
+	 * Constructs a BoxAPIResponseException with a specified message.
+	 * @param message	a message explaining why the exception occurred.
+	 */
+	public BoxAPIResponseException(String message) {
+		super(message);
+
+		this.message = message;
+		this.response = null;
+		this.responseObj = null;
+	}
+
+	/**
+	 * Constructs a BoxAPIResponseException with a detailed message for underlying exception.
+	 * @param message		a message explaining why the exception occurred.
+	 * @param responseBody	the response body returned by the Box server.
+	 */
+	public BoxAPIResponseException(String message, String responseBody) {
+		super(message);
+
+		this.message = message;
+		this.response = responseBody;
+		this.responseObj = null;
+	}
+
+	/**
+	 * Constructs a BoxAPIResponseException that wraps another underlying exception with details about the server's response.
+	 * @param message			a message explaining why the exception occurred.
+	 * @param responseBody		the response body returned by the Box server.
+	 * @param response			the response returned by the Box server.
+	 */
+	public BoxAPIResponseException(String message, String responseBody, BoxAPIResponse response) {
+		super(message);
+
+		this.message = message;
+		this.response = responseBody;
+		this.responseObj = response;
+	}
 
 }
--- src/main/java/com/box/sdk/BoxValidationException.java
+++ src/main/java/com/box/sdk/BoxValidationException.java
@@ -4,6 +4,7 @@ package com.box.sdk;
  * Thrown to indicate that an error occurred while validating a Box API call.
  */
 public class BoxValidationException {
+
 	private final String message;
 
 	/**
@@ -11,9 +12,6 @@ public class BoxValidationException {
 	 * @param message a message explaining why the exception occurred.
 	 */
 	public BoxValidationException(String message) {
-
 		this.message = message;
 	}
-
-	
 }
