f2e41d9d392d5e216363aa40be2fd5ef492157ce
carycheng <ccheng@box.com>
2018-11-01 17:16:29 -0700

    changed to class Boolean

--- src/main/java/com/box/sdk/BoxLegalHoldPolicy.java
+++ src/main/java/com/box/sdk/BoxLegalHoldPolicy.java
@@ -356,7 +356,7 @@ public class BoxLegalHoldPolicy extends BoxResource {
         /**
          * @see #getIsOngoing()
          */
-        private boolean isOngoing;
+        private Boolean isOngoing;
 
         /**
          * Constructs an empty Info object.
