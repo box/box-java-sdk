1cddc1385b4c87355532c14250bf15c7d0aadc77
carycheng <ccheng@box.com>
2019-01-14 16:08:58 -0800

    add missing fields to task and add ability to get fields for tasks

--- src/test/java/com/box/sdk/BoxFolderTest.java
+++ src/test/java/com/box/sdk/BoxFolderTest.java
@@ -672,6 +672,26 @@ public class BoxFolderTest {
 
     @Test
     @Category(UnitTest.class)
+    public void testGetRestrictedCollaborationFieldSucceeds() throws IOException {
+        String result = "";
+        final String folderID = "12345";
+        final String folderInfoURL = "/folders/" + folderID;
+
+        result = TestConfig.getFixture("BoxFolder/GetFolderInfoForCollaborationRestriction200");
+
+        WIRE_MOCK_CLASS_RULE.stubFor(WireMock.get(WireMock.urlPathEqualTo(folderInfoURL))
+                .willReturn(WireMock.aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(result)));
+
+        BoxFolder folder = new BoxFolder(this.api, folderID);
+        BoxFolder.Info info = folder.getInfo();
+
+        Assert.assertTrue(info.getIsCollaborationRestrictedToEnterprise());
+    }
+
+    @Test
+    @Category(UnitTest.class)
     public void testUpdateFolderInfoSucceedsAndSendsCorrectJson() throws IOException {
         String result = "";
         final String folderID = "12345";
