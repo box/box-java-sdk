edc650b49d9de333ab0bdcb92f1e598f197a2317
Sujay Garlanka <sujay.garlanka@gmail.com>
2019-12-13 19:14:24 -0800

    Update src/main/java/com/box/sdk/BoxResourceIterable.java
    Co-Authored-By: Patrick Simon <patrick@box.com>

--- src/main/java/com/box/sdk/BoxResourceIterable.java
+++ src/main/java/com/box/sdk/BoxResourceIterable.java
@@ -80,7 +80,7 @@ public abstract class BoxResourceIterable<T> implements Iterable<T> {
      * @param api
      *            the API connection to be used by the resource
      * @param url
-     *            end-point with paging support
+     *            endpoint with paging support
      * @param limit
      *            the maximum number of items to return in a page
      * @param marker
