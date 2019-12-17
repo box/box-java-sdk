e0d1ff61d7a4da4bd758a273b4dd97988df06771
Patrick Simon <patrick@box.com>
2019-12-16 21:31:39 -0800

    Fix checkstyle bypass

--- src/main/java/com/box/sdk/MetadataTemplate.java
+++ src/main/java/com/box/sdk/MetadataTemplate.java
@@ -328,7 +328,7 @@ public class MetadataTemplate extends BoxJSONObject {
      * @param marker Optional: the marker to use for requesting the next page
      * @return An iterable of BoxItem.Info search results
      */
-    @SuppressWarnings("checkstyle:methodlength")
+    @SuppressWarnings("checkstyle:MethodLength")
     public static BoxResourceIterable<BoxItem.Info> executeMetadataQuery(final BoxAPIConnection api,
                                             String from, String query, JsonObject queryParameters,
                                             String ancestorFolderId, String indexName,
