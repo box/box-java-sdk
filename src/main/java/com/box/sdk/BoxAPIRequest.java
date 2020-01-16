467519e89e0a0f985fc80349d4a12443c6ea1631
Patrick Simon <patrick@box.com>
2020-01-15 16:28:29 -0800

    Implement String rep of API Request Body

--- src/main/java/com/box/sdk/BoxAPIRequest.java
+++ src/main/java/com/box/sdk/BoxAPIRequest.java
@@ -1,6 +1,7 @@
 package com.box.sdk;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -430,15 +431,32 @@ public class BoxAPIRequest {
 
     /**
      * Returns a String representation of this request's body used in {@link #toString}. This method returns
-     * null by default.
-     *
-     * <p>A subclass may want override this method if the body can be converted to a String for logging or debugging
-     * purposes.</p>
-     *
+     * null by default.     *
      * @return a String representation of this request's body.
      */
     protected String bodyToString() {
-        return null;
+        if (this.body == null) {
+            return null;
+        }
+
+        try {
+            ByteArrayOutputStream result = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = this.body.read(buffer)) != -1) {
+                result.write(buffer, 0, length);
+            }
+
+            return result.toString(StandardCharsets.UTF_8.name());
+        } catch (IOException e) {
+            throw new BoxAPIException("Error reading request body.", e);
+        } finally {
+            try {
+                this.resetBody();
+            } catch (IOException e) {
+                throw new BoxAPIException("Error resetting request body Stream after read.", e);
+            }
+        }
     }
 
     /**
