1e3abcb1dacd0db7eaeabca90f6d4571e2447283
Sujay Garlanka <sujay.garlanka@gmail.com>
2020-04-09 12:54:24 -0400

    Add path parameter sanitization (#790)

--- src/main/java/com/box/sdk/URLTemplate.java
+++ src/main/java/com/box/sdk/URLTemplate.java
@@ -2,11 +2,14 @@ package com.box.sdk;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.regex.Pattern;
 
 /**
  * A template class to build URLs from base URL, path, URL parameters and Query String.
  */
 public class URLTemplate {
+    private static final Pattern NUMERIC = Pattern.compile("^[0-9]*$");
+    private static final Pattern ALPHA_NUMERIC = Pattern.compile("^[a-zA-Z0-9!@#$%^&*()_+\\-]*$");
     private String template;
 
     /**
@@ -18,38 +21,102 @@ public class URLTemplate {
     }
 
     /**
-     * Build a URL with URL Parameters.
+     * Build a URL with numeric URL Parameters.
      * @param base base URL
      * @param values URL parameters
      * @return URL
      */
     public URL build(String base, Object... values) {
+        for (Object value: values) {
+            String valueString = String.valueOf(value);
+            Boolean test = NUMERIC.matcher(valueString).matches();
+            if (!NUMERIC.matcher(valueString).matches()) {
+                assert false : "An invalid path parameter passed in. It must be numeric.";
+            }
+        }
         String urlString = String.format(base + this.template, values);
 
         URL url = null;
         try {
             url = new URL(urlString);
         } catch (MalformedURLException e) {
-            assert false : "An invalid URL template indicates a bug in the SDK.";
+            assert false : "A valid URL could not be constructed from the provided parameters.";
         }
 
         return url;
     }
 
     /**
-     * Build a URL with Query String and URL Parameters.
+     * Build a URL with alphanumeric URL Parameters.
+     * @param base base URL
+     * @param values URL parameters
+     * @return URL
+     */
+    public URL buildAlpha(String base, Object... values) {
+        for (Object value: values) {
+            String valueString = String.valueOf(value);
+            Boolean test = ALPHA_NUMERIC.matcher(valueString).matches();
+            if (!ALPHA_NUMERIC.matcher(valueString).matches()) {
+                assert false : "An invalid path parameter passed in. It must be alphanumeric.";
+            }
+        }
+        String urlString = String.format(base + this.template, values);
+
+        URL url = null;
+        try {
+            url = new URL(urlString);
+        } catch (MalformedURLException e) {
+            assert false : "A valid URL could not be constructed from the provided parameters.";
+        }
+
+        return url;
+    }
+
+    /**
+     * Build a URL with Query String and numeric URL Parameters.
      * @param base base URL
      * @param queryString query string
      * @param values URL Parameters
      * @return URL
      */
     public URL buildWithQuery(String base, String queryString, Object... values) {
+        for (Object value: values) {
+            String valueString = String.valueOf(value);
+            if (!NUMERIC.matcher(valueString).matches()) {
+                assert false : "An invalid path param passed in. It must be numeric.";
+            }
+        }
         String urlString = String.format(base + this.template, values) + queryString;
         URL url = null;
         try {
             url = new URL(urlString);
         } catch (MalformedURLException e) {
-            assert false : "An invalid URL template indicates a bug in the SDK.";
+            assert false : "A valid URL could not be constructed from the provided parameters.";
+        }
+
+        return url;
+    }
+
+    /**
+     * Build a URL with Query String and alphanumeric URL Parameters.
+     * @param base base URL
+     * @param queryString query string
+     * @param values URL Parameters
+     * @return URL
+     */
+    public URL buildAlphaWithQuery(String base, String queryString, Object... values) {
+        for (Object value: values) {
+            String valueString = String.valueOf(value);
+            if (!ALPHA_NUMERIC.matcher(valueString).matches()) {
+                assert false : "An invalid path param passed in. It must be alphanumeric.";
+            }
+        }
+        String urlString = String.format(base + this.template, values) + queryString;
+        URL url = null;
+        try {
+            url = new URL(urlString);
+        } catch (MalformedURLException e) {
+            assert false : "A valid URL could not be constructed from the provided parameters.";
         }
 
         return url;
---
+++ src/test/java/com/box/sdk/BoxURLTemplateTest.java
@@ -0,0 +1,73 @@
+package com.box.sdk;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.net.URL;
+
+/**
+ * Unit tests for {@link URLTemplate}.
+ */
+public class BoxURLTemplateTest {
+    public static final URLTemplate ADD_COMMENT_URL_TEMPLATE = new URLTemplate("test/%s");
+    public static final String BASE_URL = TestConfig.getAPIConnection().getBaseURL();
+
+    /**
+    * Unit test for {@link URLTemplate#build(String...)}
+    */
+    @Test
+    @Category(UnitTest.class)
+    public void testBuildSucceeds() {
+        String param = "12345";
+        URLTemplate template = new URLTemplate("test/%s");
+        URL url = template.build(BASE_URL, param);
+        String expectedURL = BASE_URL + "test/" + param;
+        Assert.assertEquals(url.toString(), expectedURL);
+    }
+
+    /**
+    * Unit test for {@link URLTemplate#build(String...)}
+    */
+    @Test
+    @Category(UnitTest.class)
+    public void testBuildFails() {
+        URLTemplate template = new URLTemplate("test/%s");
+        try {
+            URL url = template.build(BASE_URL, "1fdsa45");
+        } catch (AssertionError e) {
+            Assert.assertEquals("An invalid path parameter passed in. It must be numeric.", e.getMessage());
+            return;
+        }
+        Assert.fail("Never threw an AssertionError");
+    }
+
+    /**
+    * Unit test for {@link URLTemplate#buildAlpha(String...)}
+    */
+    @Test
+    @Category(UnitTest.class)
+    public void testBuildAlphaSucceeds() {
+        String param = "$123-45_67";
+        URLTemplate template = new URLTemplate("test/%s");
+        URL url = template.buildAlpha(BASE_URL, param);
+        String expectedURL = BASE_URL + "test/" + param;
+        Assert.assertEquals(url.toString(), expectedURL);
+    }
+
+    /**
+    * Unit test for {@link URLTemplate#buildAlpha(String...)}
+    */
+    @Test
+    @Category(UnitTest.class)
+    public void testBuildAlphaFails() {
+        URLTemplate template = new URLTemplate("test/%s");
+        try {
+            URL url = template.buildAlpha(BASE_URL, "1234.45/43/5");
+        } catch (AssertionError e) {
+            Assert.assertEquals("An invalid path parameter passed in. It must be alphanumeric.", e.getMessage());
+            return;
+        }
+        Assert.fail("Never threw an AssertionError");
+    }
+}
