5a1581ee17ab4e0e2b38497429741bba1e7a91e7
Sujay Garlanka <sujay.garlanka@gmail.com>
2020-07-13 15:46:57 -0400

    Add zip functionality (#825)

---
+++ doc/zip_download.md
@@ -0,0 +1,56 @@
+Zip Download
+======
+
+Allows you to create a temporary zip file of Box files and folders and download them.
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+
+
+- [Create a Zip File](#create-a-zip-file)
+- [Download a Zip File](#download-a-zip-file)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
+Create a Zip File
+---------------
+
+Calling [`BoxZip.create(String name, List<BoxZipItem> items)`][create-a-zip-file] will let you create a new zip file with the specified name and 
+with the specified items and will return a `BoxZipInfo` object with the download link. This file does not show up in your Box account, but will be temporarily 
+available for download.
+
+```java
+ArrayList<BoxZipItem> items = new ArrayList<BoxZipItem>();
+BoxZipItem file = new BoxZipItem("file", "12345");
+BoxZipItem folder = new BoxZipItem("folder", "156472");
+items.add(file);
+items.add(folder);
+BoxZip zip = new BoxZip(api);
+BoxZipInfo zipInfo = zip.create("Awesome Zip File", items);
+```
+
+[create-a-zip-file]: http://opensource.box.com/box-java-sdk/javadoc/com/box/sdk/BoxZip.html
+
+Download a Zip File
+------------
+
+Calling [`BoxZip.download(String name, List<BoxZipItem> items, OutputStream output)`][download-a-zip-file] will let you create a new zip file 
+with the specified name and with the specified items and download it to the stream that is passed in. The return object is `BoxZipDownloadStatus` 
+object that containes information about the download, including whether it was successful. The created zip file does not show up in your Box account.
+
+```java
+ArrayList<BoxZipItem> items = new ArrayList<BoxZipItem>();
+BoxZipItem file = new BoxZipItem("file", "12345");
+BoxZipItem folder = new BoxZipItem("folder", "156472");
+items.add(file);
+items.add(folder);
+BoxZip zip = new BoxZip(api);
+FileOutputStream stream = new FileOutputStream();
+BoxZipDownloadStatus zipDownloadStatus = new BoxZip(api).download("Another Awesome Zip File", items, stream);
+stream.close();
+if (zipDownloadStatus.getState() == BoxZipDownloadStatus.State.SUCCEEDED) {
+    System.out.println("Zip downloaded successfully");
+}
+```
+
+[download-a-zip-file]: http://opensource.box.com/box-java-sdk/javadoc/com/box/sdk/BoxZip.html
---
+++ src/main/java/com/box/sdk/BoxZip.java
@@ -0,0 +1,118 @@
+package com.box.sdk;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.List;
+
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+
+/**
+ * Provides methods to allow users to download multiple files and folders as a single zip file. Users can download
+ * up to either 32GB or 10,000 files in one batch (whichever limitation is hit first) as a single zip file.
+ */
+public class BoxZip {
+    /**
+     * Zip URL Template.
+     */
+    public static final URLTemplate ZIP_URL_TEMPLATE = new URLTemplate("zip_downloads");
+    /**
+     * Zip Download URL Template.
+     */
+    public static final URLTemplate ZIP_DOWNLOAD_URL_TEMPLATE = new URLTemplate("zip_downloads/%s/content");
+    private static final int BUFFER_SIZE = 8192;
+    private final BoxAPIConnection api;
+
+    /**
+     * Constructs a Zip to be used by everything.
+     *
+     * @param api the API connection to be used by the Zip.
+     */
+    public BoxZip(BoxAPIConnection api) {
+        this.api = api;
+    }
+
+    /**
+     * Creates a zip of multiple files and folders.
+     *
+     * @param name  the name of the zip file to be created
+     * @param items list of files or folders to be part of the created zip
+     * @return      information about the created zip file
+     */
+    public BoxZipInfo create(String name, List<BoxZipItem> items) {
+        JsonArray itemsArray = new JsonArray();
+        for (BoxZipItem item : items) {
+            itemsArray.add(item.getJSONObject());
+        }
+        JsonObject requestJSON = new JsonObject();
+        requestJSON.add("items", itemsArray);
+        requestJSON.add("download_file_name", name);
+
+        URL url = ZIP_URL_TEMPLATE.build(this.getAPI().getBaseURL());
+        BoxAPIRequest request = new BoxAPIRequest(this.getAPI(), url, "POST");
+        request.setBody(requestJSON.toString());
+        BoxJSONResponse response = (BoxJSONResponse) request.send();
+        JsonObject responseJSON = JsonObject.readFrom(response.getJSON());
+
+        BoxZipInfo zipInfo = new BoxZipInfo(responseJSON);
+        return zipInfo;
+    }
+
+    /**
+     * Creates a zip and downloads it to a given OutputStream.
+     *
+     * @param name   the name of the zip file to be created
+     * @param items  list of files or folders to be part of the created zip
+     * @param output the stream to where the zip file will be written.
+     * @return       information about status of the download
+     */
+    public BoxZipDownloadStatus download(String name, List<BoxZipItem> items, OutputStream output) {
+        return this.download(name, items, output, null);
+    }
+
+    /**
+     * Creates a zip and downloads its contents its to a given OutputStream.
+     *
+     * @param name     the name of the zip file to be created
+     * @param items    list of files or folders to be part of the created zip
+     * @param output   the stream to where the zip file will be written.
+     * @param listener a listener for monitoring the download's progress.
+     * @return         information about status of the download
+     */
+    public BoxZipDownloadStatus download(String name, List<BoxZipItem> items, OutputStream output,
+                                         ProgressListener listener) {
+        BoxZipInfo zipInfo = this.create(name, items);
+        BoxAPIRequest request = new BoxAPIRequest(this.getAPI(), zipInfo.getDownloadURL(), "GET");
+        BoxAPIResponse response = request.send();
+        InputStream input = response.getBody(listener);
+
+        byte[] buffer = new byte[BUFFER_SIZE];
+        try {
+            int n = input.read(buffer);
+            while (n != -1) {
+                output.write(buffer, 0, n);
+                n = input.read(buffer);
+            }
+        } catch (IOException e) {
+            throw new BoxAPIException("Couldn't connect to the Box API due to a network error.", e);
+        } finally {
+            response.disconnect();
+        }
+        BoxAPIRequest statusRequest = new BoxAPIRequest(this.getAPI(), zipInfo.getStatusURL(), "GET");
+        BoxJSONResponse statusResponse = (BoxJSONResponse) statusRequest.send();
+        JsonObject statusResponseJSON = JsonObject.readFrom(statusResponse.getJSON());
+        BoxZipDownloadStatus downloadStatus = new BoxZipDownloadStatus(statusResponseJSON);
+        return downloadStatus;
+    }
+
+    /**
+     * Gets the API connection used by this resource.
+     *
+     * @return the API connection used by this resource.
+     */
+    public BoxAPIConnection getAPI() {
+        return this.api;
+    }
+}
