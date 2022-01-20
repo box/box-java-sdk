616a7cc4fd178caafdd447f6930b5ff506751a64
Kamil Berdychowski <kberdychowski@box.com>
2022-01-20 18:13:00 +0100

    chore: changed layout

--- src/test/java/com/box/sdk/BoxAPIRequestTest.java
+++ src/test/java/com/box/sdk/BoxAPIRequestTest.java
@@ -89,8 +89,8 @@ public class BoxAPIRequestTest {
         request.send();
 
         String headerRegex = "agent=box-java-sdk/\\d\\.\\d+\\.\\d+(-[a-zA-Z]+)?; env=Java/\\d+\\.\\d+\\.\\d+(_\\d+)?";
-        RequestPatternBuilder requestPatternBuilder = RequestPatternBuilder.newRequestPattern().withHeader("X-Box-UA",
-            matching(headerRegex));
+        RequestPatternBuilder requestPatternBuilder = RequestPatternBuilder.newRequestPattern()
+            .withHeader("X-Box-UA", matching(headerRegex));
         verify(requestPatternBuilder);
     }
 
