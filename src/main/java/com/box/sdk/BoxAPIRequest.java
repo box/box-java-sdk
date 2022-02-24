f9e5de6d188ea61706432f77707406feef280c92
Kamil Berdychowski <kberdychowski@box.com>
2022-02-24 13:39:50 +0100

    chore: Fix linter problems 3

--- src/main/java/com/box/sdk/BoxAPIRequest.java
+++ src/main/java/com/box/sdk/BoxAPIRequest.java
@@ -193,16 +193,11 @@ public class BoxAPIRequest {
         String message = apiException.getMessage();
         String errorCode = "";
 
-        try {
-            JsonObject responseBody = Json.parse(response).asObject();
-            if (responseBody.get("code") != null) {
-                errorCode = responseBody.get("code").toString();
-            }
-            if (responseBody.get("error") != null) {
-                errorCode = responseBody.get("error").toString();
-            }
-        } catch (Exception e) {
-            // we have to ignore the exception
+        JsonObject responseBody = Json.parse(response).asObject();
+        if (responseBody.get("code") != null) {
+            errorCode = responseBody.get("code").toString();
+        } else if (responseBody.get("error") != null) {
+            errorCode = responseBody.get("error").toString();
         }
 
         boolean isClockSkewError = responseCode == 400
