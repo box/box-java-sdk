f9a6135924f1d6aa85cd50057420ab19e5f15dfc
Kamil Berdychowski <kberdychowski@box.com>
2022-05-10 15:27:37 +0200

    docs: added docs for AvatarUploadResponse.java

--- src/main/java/com/box/sdk/AvatarUploadResponse.java
+++ src/main/java/com/box/sdk/AvatarUploadResponse.java
@@ -1,27 +1,42 @@
 package com.box.sdk;
 
 /**
- *
+ * Upload avatar response
  */
 public class AvatarUploadResponse {
     private final String small;
     private final String large;
     private final String preview;
 
-    public AvatarUploadResponse(String small, String large, String preview) {
+    AvatarUploadResponse(String small, String large, String preview) {
         this.small = small;
         this.large = large;
         this.preview = preview;
     }
 
+    /**
+     * URL with the small representation of Avatar.
+     *
+     * @return URL with the small representation of Avatar
+     */
     public String getSmall() {
         return small;
     }
 
+    /**
+     * URL with the large representation of Avatar.
+     *
+     * @return URL with the large representation of Avatar
+     */
     public String getLarge() {
         return large;
     }
 
+    /**
+     * URL with the preview representation of Avatar.
+     *
+     * @return URL with the preview representation of Avatar
+     */
     public String getPreview() {
         return preview;
     }
