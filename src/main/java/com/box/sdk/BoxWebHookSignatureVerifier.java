86862005744a07eabb097677b0bed803599d3a84
Minh Nguyen Cong <mcong@box.com>
2025-06-24 11:09:02 +0200

    fix: Fix compare message webhook message signature

--- src/main/java/com/box/sdk/BoxWebHookSignatureVerifier.java
+++ src/main/java/com/box/sdk/BoxWebHookSignatureVerifier.java
@@ -4,7 +4,6 @@ import com.box.sdk.internal.pool.MacPool;
 import java.nio.charset.Charset;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.Map;
