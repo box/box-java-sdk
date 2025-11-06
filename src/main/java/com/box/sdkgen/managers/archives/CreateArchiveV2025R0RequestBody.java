f50d4d283e06c2004580f2763ae12be78cab0e12
box-sdk-build <sdk-build@box.com>
2025-11-06 03:11:57 -0800

    docs: Modify Archive API (box/box-openapi#563)

--- src/main/java/com/box/sdkgen/managers/archives/CreateArchiveV2025R0RequestBody.java
+++ src/main/java/com/box/sdkgen/managers/archives/CreateArchiveV2025R0RequestBody.java
@@ -1,5 +1,6 @@
 package com.box.sdkgen.managers.archives;
 
+import com.box.sdkgen.internal.NullableFieldTracker;
 import com.box.sdkgen.internal.SerializableObject;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -11,13 +12,36 @@ public class CreateArchiveV2025R0RequestBody extends SerializableObject {
   /** The name of the archive. */
   protected final String name;
 
+  /** The description of the archive. */
+  protected String description;
+
+  /** The ID of the storage policy that the archive is assigned to. */
+  @JsonProperty("storage_policy_id")
+  protected String storagePolicyId;
+
   public CreateArchiveV2025R0RequestBody(@JsonProperty("name") String name) {
     super();
     this.name = name;
   }
 
+  protected CreateArchiveV2025R0RequestBody(Builder builder) {
+    super();
+    this.name = builder.name;
+    this.description = builder.description;
+    this.storagePolicyId = builder.storagePolicyId;
+    markNullableFieldsAsSet(builder.getExplicitlySetNullableFields());
+  }
+
   public String getName() {
     return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String getStoragePolicyId() {
+    return storagePolicyId;
   }
 
   @Override
@@ -29,16 +53,58 @@ public class CreateArchiveV2025R0RequestBody extends SerializableObject {
       return false;
     }
     CreateArchiveV2025R0RequestBody casted = (CreateArchiveV2025R0RequestBody) o;
-    return Objects.equals(name, casted.name);
+    return Objects.equals(name, casted.name)
+        && Objects.equals(description, casted.description)
+        && Objects.equals(storagePolicyId, casted.storagePolicyId);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(name);
+    return Objects.hash(name, description, storagePolicyId);
   }
 
   @Override
   public String toString() {
-    return "CreateArchiveV2025R0RequestBody{" + "name='" + name + '\'' + "}";
+    return "CreateArchiveV2025R0RequestBody{"
+        + "name='"
+        + name
+        + '\''
+        + ", "
+        + "description='"
+        + description
+        + '\''
+        + ", "
+        + "storagePolicyId='"
+        + storagePolicyId
+        + '\''
+        + "}";
+  }
+
+  public static class Builder extends NullableFieldTracker {
+
+    protected final String name;
+
+    protected String description;
+
+    protected String storagePolicyId;
+
+    public Builder(String name) {
+      super();
+      this.name = name;
+    }
+
+    public Builder description(String description) {
+      this.description = description;
+      return this;
+    }
+
+    public Builder storagePolicyId(String storagePolicyId) {
+      this.storagePolicyId = storagePolicyId;
+      return this;
+    }
+
+    public CreateArchiveV2025R0RequestBody build() {
+      return new CreateArchiveV2025R0RequestBody(this);
+    }
   }
 }
