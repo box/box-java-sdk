cacc729dc246c914eb1a6d8281f4261c96711dc1
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2025-12-16 16:47:11 +0100

    feat: Treat `nullable` fields as Optional (box/box-codegen#906) (#1634)
    Co-authored-by: box-sdk-build <box-sdk-build@box.com>

---
+++ src/main/java/com/box/sdkgen/internal/OneOfSeven.java
@@ -0,0 +1,65 @@
+package com.box.sdkgen.internal;
+
+import com.box.sdkgen.serialization.json.JsonManager;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import java.io.IOException;
+
+public class OneOfSeven<T0, T1, T2, T3, T4, T5, T6> extends SerializableObject {
+  protected static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+  protected T0 value0;
+  protected T1 value1;
+  protected T2 value2;
+  protected T3 value3;
+  protected T4 value4;
+  protected T5 value5;
+  protected T6 value6;
+
+  public OneOfSeven(T0 value0, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6) {
+    this.value0 = value0;
+    this.value1 = value1;
+    this.value2 = value2;
+    this.value3 = value3;
+    this.value4 = value4;
+    this.value5 = value5;
+    this.value6 = value6;
+  }
+
+  public static class OneOfSevenSerializer extends JsonSerializer<OneOfSeven<?, ?, ?, ?, ?, ?, ?>> {
+
+    public OneOfSevenSerializer() {
+      super();
+    }
+
+    @Override
+    public void serialize(
+        OneOfSeven<?, ?, ?, ?, ?, ?, ?> itemEntryField,
+        JsonGenerator gen,
+        SerializerProvider provider)
+        throws IOException {
+      if (itemEntryField.value0 != null) {
+        JsonManager.WRITER.writeValue(gen, itemEntryField.value0);
+      }
+      if (itemEntryField.value1 != null) {
+        JsonManager.WRITER.writeValue(gen, itemEntryField.value1);
+      }
+      if (itemEntryField.value2 != null) {
+        JsonManager.WRITER.writeValue(gen, itemEntryField.value2);
+      }
+      if (itemEntryField.value3 != null) {
+        JsonManager.WRITER.writeValue(gen, itemEntryField.value3);
+      }
+      if (itemEntryField.value4 != null) {
+        JsonManager.WRITER.writeValue(gen, itemEntryField.value4);
+      }
+      if (itemEntryField.value5 != null) {
+        JsonManager.WRITER.writeValue(gen, itemEntryField.value5);
+      }
+      if (itemEntryField.value6 != null) {
+        JsonManager.WRITER.writeValue(gen, itemEntryField.value6);
+      }
+    }
+  }
+}
