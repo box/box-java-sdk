a31b4f8ca7f592991f968f66f13d9cafdeca1ecf
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2026-09-03 16:11:07 +0200

    feat(boxsdkgen): add Japanese ZIP code validation to sign request (box/box-openapi#615) (#1975)

--- src/main/java/com/box/sdkgen/schemas/templatesignerinput/TemplateSignerInput.java
+++ src/main/java/com/box/sdkgen/schemas/templatesignerinput/TemplateSignerInput.java
@@ -13,6 +13,7 @@ import com.box.sdkgen.schemas.signrequestsignerinputnumberwithperiodvalidation.S
 import com.box.sdkgen.schemas.signrequestsignerinputssnvalidation.SignRequestSignerInputSsnValidation;
 import com.box.sdkgen.schemas.signrequestsignerinputvalidation.SignRequestSignerInputValidation;
 import com.box.sdkgen.schemas.signrequestsignerinputzip4validation.SignRequestSignerInputZip4Validation;
+import com.box.sdkgen.schemas.signrequestsignerinputzipjpvalidation.SignRequestSignerInputZipjpValidation;
 import com.box.sdkgen.schemas.signrequestsignerinputzipvalidation.SignRequestSignerInputZipValidation;
 import com.box.sdkgen.serialization.json.EnumWrapper;
 import com.fasterxml.jackson.annotation.JsonFilter;
@@ -386,6 +387,11 @@ public class TemplateSignerInput extends SignRequestPrefillTag {
     }
 
     public Builder validation(SignRequestSignerInputZip4Validation validation) {
+      this.validation = new SignRequestSignerInputValidation(validation);
+      return this;
+    }
+
+    public Builder validation(SignRequestSignerInputZipjpValidation validation) {
       this.validation = new SignRequestSignerInputValidation(validation);
       return this;
     }
